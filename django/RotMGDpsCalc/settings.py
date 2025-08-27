5f0154973a496149d901bda78a94884b944d90ea
Leon <leonpa@hotmail.co.uk>
2025-08-27 16:58:02 +0100

    main

--- django/RotMGDpsCalc/settings.py
+++ django/RotMGDpsCalc/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+q46+eqv#^h65kq9i&7ormk+o$bdr5$h_!7)1*@5vc=_7p&w7'
+# SECRET_KEY = 'django-insecure--+q46+eqv#^h65kq9i&7ormk+o$bdr5$h_!7)1*@5vc=_7p&w7'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
