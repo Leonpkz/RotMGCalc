e6a63e4db2e87475c5c71a6b0269bafc9dfab7a0
Leon <leonpa@hotmail.co.uk>
2025-08-29 14:33:05 +0100

    spritemapping movement

---
+++ project/utils/spritemapping.py
@@ -0,0 +1,71 @@
+import json
+import flatbuffers
+import AnimatedSpriteSheet, SpriteSheet, SpriteSheetRoot, Sprite, Position, Color
+
+filepath = r'C:\Code\exalt-extractor-main\output\xml\spritesheetf.bin'
+
+'''
+This module considers the use of flatc (https://flatbuffers.dev/) to decode the binary file
+
+This is done decoding the spritesheetf binary from the unity game files - if you are using the exalt extractor tool the 
+file will be named spritesheetf.xml you can rename the file, and then using the sprites.fbs file provided you can 
+generate the binary file using the following command:
+
+          ./flatc.exe --python sprites.fbs
+
+This will generate the necessary python scripts used to map the spritesheet to a JSON, if the schema (sprites.fbs) 
+changes in future you won't be able to decode the spritesheef file as the exact "table" and "struct" needs to be known. 
+'''
+
+# Read binary file for spritesheet and return a dictionary containing the sprite data
+def sprite_to_dict(sprite):
+	pos = sprite.Position()
+	mask_pos = sprite.MaskPosition()
+	color = sprite.Color()
+
+	# list comprehension on position and color values decreased file size by 20MB
+	return {
+		"name": sprite.Name().decode('utf-8'),
+		"index": sprite.Index(),
+		"atlasId": sprite.AtlasId(),
+		"position": [getattr(pos, attr)() for attr in ['X', 'Y', 'W', 'H']],
+		"mask_position": [getattr(mask_pos, attr)() for attr in ['X', 'Y', 'W', 'H']],
+		"color": [getattr(color, attr)() for attr in ['R', 'G', 'B', 'A']],
+		"transparent": sprite.IsTransparent()
+	}
+
+
+def load_spritesheet(file_path):
+	with open(file_path, "rb") as f:
+		data = f.read()
+
+	buf = bytearray(data)
+	rootsheet = SpriteSheetRoot.SpriteSheetRoot.GetRootAsSpriteSheetRoot(buf, 0)
+	return rootsheet
+
+
+if __name__ == "__main__":
+	root = load_spritesheet(filepath)
+
+	# Return dictionary for all spritesheets
+	root_dict = dict(spritesheets=[
+		{
+			"name": (sheet := root.Sprites(i)).Name().decode('utf-8'),
+			"atlasId": sheet.AtlasId(),
+			"sprites": [sprite_to_dict(sheet.Sprites(j)) for j in range(sheet.SpritesLength())]
+		}
+		for i in range(root.SpritesLength())
+	], animated_sprites=[
+		{
+			"name": (anim := root.AnimatedSprites(i)).Name().decode('utf-8'),
+			"index": anim.Index(),
+			"set": anim.Set(),
+			"direction": anim.Direction(),
+			"action": anim.Action(),
+			"sprite": sprite_to_dict(anim.Sprite())
+		}
+		for i in range(root.AnimatedSpritesLength())
+	])
+
+with open("spritesheet.json", "w") as f:
+	json.dump(root_dict, f, indent=2)
