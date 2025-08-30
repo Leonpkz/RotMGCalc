5d9fb42cd21c7dd2efa0fc55d8f099eafe850321
Leon <leonpa@hotmail.co.uk>
2025-08-30 19:29:25 +0100

    Update settings.py

--- django/RotMGDpsCalc/settings.py
+++ django/RotMGDpsCalc/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure--+q46+eqv#^h65kq9i&7ormk+o$bdr5$h_!7)1*@5vc=_7p&w7'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
