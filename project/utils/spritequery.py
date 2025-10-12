99de979944383f69bb23386cfa1380ffe4e7899a
Leon <leonpa@hotmail.co.uk>
2025-10-12 21:58:20 +0100

    slight image updating

--- project/utils/spritequery.py
+++ project/utils/spritequery.py
@@ -1,5 +1,6 @@
 import json
 import os
+import PIL.Image
 
 # spritesheet extracted from spritemapping.py
 SPRITE_SHEET_JSON = os.environ.get('SPRITE_SHEET_JSON')
@@ -9,3 +10,7 @@ with open(SPRITE_SHEET_JSON) as f:
 
 for row in sprites:
     print(row)
+
+spriteSheet = PIL.Image.open(r"c:\Code\RotMGCalc\localfiles\spritesheets\mapObjects.png")
+# spriteSheet.alpha_composite(dest=spriteSheet)
+spriteSheet.show()
